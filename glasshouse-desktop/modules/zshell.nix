fbe1d2222bbdcb57072283b32ad03d298a218bd5
Kyler Clay <kylerclay@proton.me>
2024-09-25 01:51:45 -0400

    Commit for generation 58

--- glasshouse-desktop/modules/zshell.nix
+++ glasshouse-desktop/modules/zshell.nix
@@ -206,7 +206,14 @@ nixswitch() {
 		git push
 	fi
 	sudo nixos-rebuild switch --flake "$HOME/sysflakes#glasshouse"
+	cd $OLDPWD
 }
+journal() {
+	# journal for keeping track of stuff I do that isn't declared in my nix config
+	[ ! -f "$HOME/loose_ends" ] && touch "$HOME/loose_ends" 
+	echo "$1" >> "$HOME/loose_ends" 
+}
+
 		'';
 
 		initExtraBeforeCompInit = ''
