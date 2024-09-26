b9b1cd1cd89b1142729c886214ffb75ad49d14b9
Kyler Clay <kylerclay@proton.me>
2024-09-26 15:59:53 -0400

    Commit for generation 105

--- glasshouse-desktop/dotfiles/packages/nixvim/config/keymaps.nix
+++ glasshouse-desktop/dotfiles/packages/nixvim/config/keymaps.nix
@@ -16,10 +16,24 @@
 			mode = "t";
 		}
 		{
-			action = "<cmd>FloatermKill rangerterm<CR>";
-			key = "<F3>";
-			mode = "t";
+			action = "<C-w>h";
+			key = "<C-h>";
+			mode = "n";
 		}
-
+		{
+			action = "<C-w>j";
+			key = "<C-j>";
+			mode = "n";
+		}
+		{
+			action = "<C-w>k";
+			key = "<C-k>";
+			mode = "n";
+		}
+		{
+			action = "<C-w>l";
+			key = "<C-l>";
+			mode = "n";
+		}	
 	];
 }
