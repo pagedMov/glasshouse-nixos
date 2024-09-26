050e9ea9e4a08670729542e87ba64dcdc2024a67
Kyler Clay <kylerclay@proton.me>
2024-09-26 17:56:55 -0400

    Commit for generation 115

--- glasshouse-desktop/dotfiles/packages/nixvim/config/plugins/nvim-tree.nix
+++ glasshouse-desktop/dotfiles/packages/nixvim/config/plugins/nvim-tree.nix
@@ -4,7 +4,6 @@
 			hijackUnnamedBufferWhenOpening = true;
 			openOnSetup = true;
 			onAttach = { __raw = "function(bufnr) local api = require('nvim-tree.api') vim.keymap.set('n', 'b', api.node.open.preview, opts('Open Preview')) end"; };
-			''; };
 			view = { 
 				side = "right";	
 				centralizeSelection = true;
