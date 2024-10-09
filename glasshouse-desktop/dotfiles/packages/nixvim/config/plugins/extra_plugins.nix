15670c77f0717a368f38d436e6c3615ec17b0a86
Kyler Clay <kylerclay@proton.me>
2024-10-09 08:41:29 -0400

    Commit for generation 215

--- glasshouse-desktop/dotfiles/packages/nixvim/config/plugins/extra_plugins.nix
+++ glasshouse-desktop/dotfiles/packages/nixvim/config/plugins/extra_plugins.nix
@@ -7,8 +7,8 @@
 			src = pkgs.fetchFromGitHub {
 				owner = "vimwiki";
 				repo = "vimwiki";
-				rev = "dev";
-				hash = "705ad1e0dded0e3b7ff5fac78547ab67c9d39bdf";
+				rev = "705ad1e0dded0e3b7ff5fac78547ab67c9d39bdf";
+				hash = "sha256-Upx29rIPwW/e7Lkmf0PNOpIACnAXIzlkfa6V1p2nYHM=";
 			};
 		})
 	];
