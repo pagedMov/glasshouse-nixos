f51df5e67b58a3f1429a18aa50bfa9232977663a
Kyler Clay <kylerclay@proton.me>
2024-10-10 04:01:35 -0400

    Commit for generation 220

--- glasshouse-desktop/home/default.nix
+++ glasshouse-desktop/home/default.nix
@@ -2,19 +2,19 @@
 
 {
 	imports = 
-		[ (import ./btop.nix) 	   ]
-	 ++ [ (import ./bat.nix) 	   ]
-	 ++ [ (import ./cava.nix) 	   ]
-	 ++ [ (import ./fuzzel.nix)    ]
-	 ++ [ (import ./gtk.nix) 	   ]
-	 ++ [ (import ./hyprland)      ]
-	 ++ [ (import ./kitty.nix)     ]
-	 ++ [ (import ./spicetify.nix) ]
-	 ++ [ (import ./starship.nix)  ]
-	 ++ [ (import ./swaync) 	   ]
-	 ++ [ (import ./theme) 		   ]
-	 ++ [ (import ./toilet) 	   ]
-	 ++ [ (import ./userpkgs.nix)  ]
-	 ++ [ (import ./waybar) 	   ]
-	 ++ [ (import ./zshell.nix)    ];
+		[ (import ./btop.nix) 	      ]
+	 ++ [ (import ./bat.nix) 	      ]
+	 ++ [ (import ./cava.nix) 	      ]
+	 ++ [ (import ./fuzzel.nix)       ]
+	 ++ [ (import ./gtk.nix) 	      ]
+	 ++ [ (import ./hyprland)         ]
+	 ++ [ (import ./kitty.nix)        ]
+	 ++ [ (import ./spicetify.nix)    ]
+	 ++ [ (import ./starship.nix)     ]
+	 ++ [ (import ./swaync/swaync.nix)]
+	 ++ [ (import ./theme) 		      ]
+	 ++ [ (import ./toilet) 	      ]
+	 ++ [ (import ./userpkgs.nix)     ]
+	 ++ [ (import ./waybar) 	      ]
+	 ++ [ (import ./zshell.nix)       ];
 }
