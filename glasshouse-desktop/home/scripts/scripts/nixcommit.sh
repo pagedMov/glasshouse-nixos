3586a5cd3ca051c5a8650086e2a9dcbf27783600
pagedMov <pagedMov@gmail.com>
2024-10-11 01:22:20 -0400

    Gen 326: some more sound file tweaks

--- glasshouse-desktop/home/scripts/scripts/nixcommit.sh
+++ glasshouse-desktop/home/scripts/scripts/nixcommit.sh
@@ -9,10 +9,12 @@ gen=$((gen + 1))
 
 git diff --quiet
 if [ $? -eq 0 ]; then
+	scheck && runbg aplay ~/media/sound/sys/warning.wav
 	echo "Nothing to commit"
 	return
 fi
 git add .
 git commit -m "Gen $gen: $@"
 git push
+scheck && runbg aplay ~/media/sound/sys/gitpush.wav
 builtin cd - || exit
