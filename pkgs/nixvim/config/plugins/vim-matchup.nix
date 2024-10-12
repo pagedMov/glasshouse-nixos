588249f10f60f7f0b80e4a3d4ad804f949061b77
pagedMov <pagedMov@gmail.com>
2024-10-12 12:38:56 -0400

    Gen 404: tweaked vim-matchup config

--- pkgs/nixvim/config/plugins/vim-matchup.nix
+++ pkgs/nixvim/config/plugins/vim-matchup.nix
@@ -2,12 +2,16 @@
 	plugins.vim-matchup = {
 		enable = true;
 		enableSurround = true;
+		textObj.enable = true;
 		motion = {
 			enable = true;
 			cursorEnd = true;
 		};
 		matchParen = {
 			hiSurroundAlways = true;
+			offscreen = {
+				method = "popup";
+			};
 		};
 		treesitterIntegration = {
 			enable = true;
