a2f4e74716cfa711cc21c2f2299a034c57f23584
pagedmov <pagedmov@gmail.com>
2024-10-17 20:00:09 -0400

    added installer for main configuration branch, and custom disko.nix disk configuration

---
+++ hosts/installer/disko-ext4-singledisk.nix
@@ -0,0 +1,60 @@
+# USAGE in your configuration.nix.
+# Update devices to match your hardware.
+{
+	device ? throw "Set this to your disk device, e.g. /dev/sda",
+				 ...
+}:
+
+{
+	disko.devices = {
+		disk = {
+			main = {
+				inherit device;
+				type = "disk";
+				content = {
+					type = "gpt";
+					partitions = {
+						boot = {
+							size = "1M";
+							type = "EF02"; # for grub MBR
+						};
+						ESP = {
+							size = "1G";
+							type = "EF00";
+							content = {
+								type = "filesystem";
+								format = "vfat";
+								mountpoint = "/boot";
+								mountOptions = [ "umask=0077" ];
+							};
+						};
+						nix = {
+							size = "35%";
+							content = {
+								type = "filesystem";
+								format = "ext4";
+								mountpoint = "/home";
+							};
+						};
+						root = {
+							size = "15%";
+							content = {
+								type = "filesystem";
+								format = "ext4";
+								mountpoint = "/";
+							};
+						};
+						home = {
+							size = "50%";
+							content = {
+								type = "filesystem";
+								format = "ext4";
+								mountpoint = "/home";
+							};
+						};
+					};
+				};
+			};
+		};
+	};
+}
